3dfd8efb2574782e77c4252ffbb08b7aa51baa93
JamesOverclock <wirayut.temsao@gmail.com>
2020-03-08 20:38:33 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:whatsappui/config/config.dart';
+import 'package:whatsappui/tabs/CallsTab.dart';
+import 'package:whatsappui/tabs/CameraTab.dart';
+import 'package:whatsappui/tabs/ChatsTab.dart';
+import 'package:whatsappui/tabs/StatusTab.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePages(),
+      theme:
+          ThemeData(brightness: Brightness.light, primaryColor: primaryColor),
+      darkTheme:
+          ThemeData(brightness: Brightness.dark, primaryColor: primaryColor),
+    );
+  }
+}
+
+class HomePages extends StatefulWidget {
+  @override
+  _HomePagesState createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      initialIndex: 1,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Whatsapp",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: false,
+          backgroundColor: primaryColor,
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.camera_alt),
+              ),
+              Tab(
+                text: "CHATS",
+              ),
+              Tab(
+                text: "STATUS",
+              ),
+              Tab(
+                text: "CALLS",
+              )
+            ],
+          ),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.more_vert),
+            )
+          ],
+        ),
+        body: TabBarView(
+          children: <Widget>[CameraTab(), ChatsTab(), StatusTab(), CallsTab()],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.chat),
+          onPressed: () {},
+          backgroundColor: primaryColor,
+        ),
+      ),
+    );
+  }
+}
